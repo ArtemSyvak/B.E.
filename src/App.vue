cb45ec29249d97b1478b18d11fa98bd7883301de
artemsyvak <artsyvak@gmail.com>
2018-11-18 21:51:17 +0200

    Done with video section and navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
       <full-page ref="fullpage" :options="options" id="fullpage">
+          <!--Video Section-->
         <div class="section">
-          First section ...
+            <VideoPlayer/>
         </div>
         <div class="section">
           Second section ...
@@ -24,10 +25,28 @@
 </template>
 
 <script>
+import VideoPlayer from "@/components/VideoPlayer";
 export default {
   name: "app",
-  data: () => {
-    return true;
+  components: {
+    VideoPlayer
+  },
+  data() {
+    return {
+      options: {
+        navigation: true,
+        licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+        anchors: [
+          "Video",
+          "Services",
+          "Portfolio",
+          "Team",
+          "Feedbacks",
+          "ContactUs"
+        ],
+        sectionsColor: ["#ffffff"]
+      }
+    };
   }
 };
 </script>
@@ -44,9 +63,91 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  #fullpage {
+    background: url("./assets/background.svg") center center no-repeat;
+    background-size: cover;
+  }
 }
-#fullpage {
-  background: url("./assets/background.svg") center center no-repeat;
-  background-size: cover;
+#fp-nav {
+  position: fixed;
+  left: 0;
+  top: 50%;
+  ul {
+    position: relative;
+    &:after,
+    &:before {
+      content: "";
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 1px 0 1px 180px;
+      border-color: transparent transparent transparent darkgray;
+      position: absolute;
+      left: -57px;
+    }
+    &:after {
+      bottom: -120px;
+      transform: rotate(90deg);
+    }
+    &:before {
+      top: -120px;
+      transform: rotate(-90deg);
+    }
+    padding-left: 30px;
+    li {
+      list-style: none;
+      a {
+        margin: 10px 0;
+        color: darkgrey;
+        display: block;
+        position: relative;
+        padding-left: 20px;
+        text-decoration: none;
+        text-transform: uppercase;
+        cursor: pointer;
+        & span:not(.fp-sr-only) {
+          display: inline-block;
+          border-radius: 50%;
+          background-color: darkgray;
+          content: "";
+          width: 4px;
+          height: 4px;
+          position: absolute;
+          top: 30%;
+          left: 0;
+          cursor: pointer;
+          webkit-transition: all 0.1s ease-in-out;
+          -moz-transition: all 0.1s ease-in-out;
+          -o-transition: all 0.1s ease-in-out;
+          transition: all 0.1s ease-in-out;
+        }
+        &:hover {
+          color: lightgray;
+          text-decoration: none;
+          &:not(.active) span:not(.fp-sr-only) {
+            width: 7px;
+            height: 7px;
+            margin-left: -2px;
+          }
+        }
+
+        &.active {
+          color: lightgray;
+          span:not(.fp-sr-only) {
+            display: inline-block;
+            background-color: yellow;
+            width: 12px;
+            height: 12px;
+            border-radius: 50%;
+            margin-left: -4px;
+            webkit-transition: all 0.1s ease-in-out;
+            -moz-transition: all 0.1s ease-in-out;
+            -o-transition: all 0.1s ease-in-out;
+            transition: all 0.1s ease-in-out;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container-fluid  video-section-container p-0">
+        <div class="video-player-container">
+            <video  class="video" :class="{pause: pause, play:!pause}" src="../assets/record_stock.mp4" type="video/mp4"></video>
+            <div class="controls">
+                <div class="progress-bar">
+                    <div class="progress-bar-circle"></div>
+                </div>
+            </div>
+            <div class="video-btn">
+                <button @click="playToggle"><i v-show="pause===true" class="fas fa-pause fa-4x"></i><i v-show="pause===false" class="fas fa-play fa-4x"></i></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "VideoPlayer",
+  data() {
+    return {
+      pause: false
+    };
+  },
+  computed: {},
+  methods: {
+    playToggle() {
+      this.pause = !this.pause;
+      let video = document.querySelector(".video");
+      video.paused ? video.play() : video.pause();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.video-player-container {
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  height: 100vh;
+  position: relative;
+  background-color: black;
+  .video {
+    margin: 0 auto;
+    height: 100%;
+    min-width: 100%;
+    min-height: 100%;
+  }
+  .controls {
+    /*display: flex;*/
+    /*position: absolute;*/
+    /*width: 100%;*/
+    /*height: 100px;*/
+    /*background-color: red;*/
+    /*bottom: 0;*/
+    /*flex-wrap: wrap;*/
+  }
+  .video-btn {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: max-content;
+    height: max-content;
+
+    button {
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+      &:focus {
+        outline: none;
+      }
+      i {
+        color: azure;
+      }
+    }
+  }
+}
+</style>
